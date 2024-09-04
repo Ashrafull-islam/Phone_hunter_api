ed2dbf51d4de973803b45ef0d826a894013b2baf
Ashrafull islam <mmmdashrafull211@gmail.com>
2024-09-03 23:09:57 -0700

    modal section done

--- js/Card.js
+++ js/Card.js
@@ -8,7 +8,7 @@ const  loadData=async(phones)=>{
 
 const Card=(phones)=>{
         const takePhones=document.getElementById('Card_Container');
-
+        takePhones.textContent='';
     phones.forEach(phone=>{
         console.log(phone);
         const div=document.createElement('div');
@@ -27,7 +27,7 @@ const Card=(phones)=>{
                 </div>
              
                   <div class="card-actions justify-center">
-                    <button class="btn btn-primary">Show Details</button>
+                    <button onclick="showModal('${phone.slug}')" class="btn btn-primary">Show Details</button>
                   </div>
                 </div>
         `;
@@ -37,6 +37,46 @@ const Card=(phones)=>{
 
 }
 
+// show modal 
+const showModal=async(id)=>{
+    const response=await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+    const data=await response.json();
+    const phnone=data.data;
+    makeModal(phnone)
+}
+// make modal 
+const makeModal=(phone)=>{
+    const takeModal=document.getElementById('Modal_box');
+    takeModal.innerText='';
+    const makeModal=document.createElement('div');
+    makeModal.innerHTML=`
+    <div class="card bg-base-100 w-full ">
+  <figure class="bg-green-100	p-4 rounded-lg">
+    <img
+      src="${phone.image}"
+      alt="Phone" />
+  </figure>
+  <div class="card-body">
+     <h2 class="card-title my-1">${phone.name}</h2>
+    <h2 class="my-1"><span class="font-semibold">Storage:</span>${phone?.mainFeatures?.storage}</h2>
+    <h2><span class="font-semibold">Display Size:</span> ${phone?.mainFeatures?.displaySize}</h2> 
+    <h2><span class="font-semibold">Chipset:</span> ${phone?.mainFeatures?.chipSet}</h2> 
+    <h2><span class="font-semibold">Memory:</span> ${phone?.mainFeatures?.memory}</h2> 
+    <h2><span class="font-semibold">Slug:</span> ${phone?.slug}</h2> 
+    <h2><span class="font-semibold">Release Date:</span> ${phone?.others?.releaseDate}</h2> 
+    <h2><span class="font-semibold">Brand:</span> ${phone?.brand}</h2> 
+ <div class="modal-action">
+      <form method="dialog">
+        <!-- if there is a button in form, it will close the modal -->
+        <button class="btn btn-outline btn-accent">Close</button>
+      </form>
+    </div>
+  </div>
+</div>
+    `;
+    takeModal.appendChild(makeModal);
+   my_modal_5.showModal();
+}
 // set loader 
 
 const loader=(isLoading)=>{
